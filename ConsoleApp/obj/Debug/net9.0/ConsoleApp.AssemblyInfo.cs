3a0190e9b047d560a04561fb01c2db22b4342dc3
Karinis24 <evelynkarinah09@gmail.com>
2025-08-27 21:03:29 -0500

    cambio merge

--- ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a3ca889f774c2b83040c2c2373adf5804d34e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b699ceb4e1a41af139bc19081dd7519d2b6b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
